0f64e30b36a8af66d6b9605a9cf8f3014280ecd1
Dhyan Anil <dhyan124853@gmail.com>
2025-10-10 23:01:15 +0530

    update .js add function to format messages.

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+function resultMessage(days, percentageRequired, action = "miss") {
+	if (days === 0) {
+		return `You can't miss any classes or the ${percentageRequired}% attendance percentage drops.`;
+	} else if (days === 1) {
+		return action === "miss" ? `You can miss 1 class and still maintain ${percentageRequired}% attendance.` : `You need to attend 1 class to maintain ${percentageRequired}% attendance.`;
+	} else {
+		return action === "miss" ? `You can miss ${days} classes and still maintain ${percentageRequired}% attendance.` : `You need to attend ${days} classes to maintain ${percentageRequired}% attendance.`;
+	}
+}
+
 const calculateButton = document.getElementById("calculate");
 const mainResult = document.getElementById("mainResult");
 
 calculateButton.addEventListener("click", () => {
-	let classesAttended = parseInt(document.getElementById("classesAttended").value);
 	let totalClasses = parseInt(document.getElementById("totalClasses").value);
+	let classesAttended = parseInt(document.getElementById("classesAttended").value);
 	let percentageRequired = parseFloat(document.getElementById("percentageRequired").value);
 
 	let currentPercentage = (classesAttended / totalClasses) * 100;
@@ -16,7 +26,7 @@ calculateButton.addEventListener("click", () => {
 				currentPercentage = (classesAttended / totalClasses) * 100;
 				daysToMiss += 1;
 			}
-			mainResult.innerHTML = `<p>You can miss <span>${daysToMiss}</span> classes and still maintain <span>${percentageRequired}% attendance</span>.</p>`;
+			mainResult.innerHTML = `${resultMessage(daysToMiss, percentageRequired, "miss")}`;
 		} else {
 			let daysToAttend = 0;
 			while (currentPercentage < percentageRequired) {
@@ -25,12 +35,14 @@ calculateButton.addEventListener("click", () => {
 				currentPercentage = (classesAttended / totalClasses) * 100;
 				daysToAttend += 1;
 			}
-			mainResult.innerHTML = `<p>Need to Attend <span>${daysToAttend}</span> classes to maintain <span>${percentageRequired}% attendance</span>.</p>`;
+			mainResult.innerHTML = `${resultMessage(daysToAttend, percentageRequired, "attend")}`;
 		}
 	} else {
-		mainResult.innerHTML = `<p>Please Enter <span>Proper</span> Values.</p>`;
+		mainResult.innerHTML = `Please enter proper values.`;
 	}
 });
 
-//TODO:
-// correct grammar in the inner.html and switch from classes to class when its 1 or vise versa.
+/*
+Todo:
+	Add html elements to make it easier to work with css in lines - 3, 5, 7, 41.
+*/
